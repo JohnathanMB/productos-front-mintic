d8ebf2d86fdfce50e53230dd9247198b4ef6da50
JohnathanMB <ja.monsalve.bello@gmail.com>
2021-10-11 00:45:07 -0500

    inicialización de formularios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleSubmit(event) {
+    //event.preventDefault();
+    let codigo = event.target.codigo.value;
+    let nombre = event.target.nombre.value;
+    let precio = event.target.precio.value;
+    let disponible = event.target.disponible.checked;
+    alert(codigo +" "+nombre+" "+precio+" "+disponible );
+  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="formulario">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Ingrese Codigo de producto" name="codigo"/><br/>
+          <input type="text" placeholder="Ingrese Nombre de producto" name="nombre"/><br/>
+          <input type="number" placeholder="Precio" name="precio"/><br/>
+          Disponible<input type="checkbox" name="disponible"/><br/>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+
+      <div className="ver-resultados">
+
+      </div>
+      
     </div>
   );
 }
