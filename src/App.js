10fd4487e7512a1fb8b60ad3fda86e98e7991ce7
JohnathanMB <ja.monsalve.bello@gmail.com>
2021-10-11 00:57:21 -0500

    refactor de clase app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom'
+import axios from 'axios';
 
-function App() {
+class App extends React.Component{
 
-  function handleSubmit(event) {
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  handleSubmit(event) {
     //event.preventDefault();
     let codigo = event.target.codigo.value;
     let nombre = event.target.nombre.value;
     let precio = event.target.precio.value;
     let disponible = event.target.disponible.checked;
     alert(codigo +" "+nombre+" "+precio+" "+disponible );
-  
   }
 
-  return (
-    <div className="App">
-      <div className="formulario">
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Ingrese Codigo de producto" name="codigo"/><br/>
-          <input type="text" placeholder="Ingrese Nombre de producto" name="nombre"/><br/>
-          <input type="number" placeholder="Precio" name="precio"/><br/>
-          Disponible<input type="checkbox" name="disponible"/><br/>
-          <input type="submit" value="Submit" />
-        </form>
+  render(){
+    return (
+      <div className="App">
+        <div className="formulario">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Ingrese Codigo de producto" name="codigo"/><br/>
+            <input type="text" placeholder="Ingrese Nombre de producto" name="nombre"/><br/>
+            <input type="number" placeholder="Precio" name="precio"/><br/>
+            Disponible<input type="checkbox" name="disponible"/><br/>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+  
+        <div className="ver-resultados">
+  
+        </div>
+        
       </div>
-
-      <div className="ver-resultados">
-
-      </div>
-      
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
