bd28e93bff5f14158893f09999976ac3a8c213ca
JohnathanMB <ja.monsalve.bello@gmail.com>
2021-10-11 21:39:21 -0500

    ajuste consumo post

--- src/App.js
+++ src/App.js
@@ -3,40 +3,21 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component{
-  state = {
-    productos: []
-  }
 
-  //Se ejecuta cuando se renderice el componente
-  componentDidMount() {
-    //Consumo get
-    axios.get(`http://localhost:3001/api/producto`)
-      .then(res => {
-        const productos = res.data.productos;
-        this.setState({ productos });
-      })
-  }
-
-  //Se ejecuta cuando se precione el botón del formulario
-  handleSubmit(event) {
-    //event.preventDefault();
-
-    //consumo post
-    axios.post(`http://localhost:3001/api/producto`, {
+  registrarProducto(event){
+    event.preventDefault();
+    
+    //consumir post con axios
+    axios.post('http://localhost:3001/api/producto',{
       codigo: event.target.codigo.value,
       nombre: event.target.nombre.value,
       precio: event.target.precio.value,
       disponible: event.target.disponible.checked
-    })
-    .then(res=>{
+    }).then(res =>{
       alert(res.data.message);
-      console.log(res);
+      console.log(res.data);
     })
-    .catch(err=>{
-      alert(err);
-      console.log(err);
-    });
-    
+
 
   }
 
@@ -44,20 +25,22 @@ class App extends React.Component{
     return (
       <div className="App">
         <div className="formulario">
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.registrarProducto}>
             <input type="text" placeholder="Ingrese Codigo de producto" name="codigo"/><br/>
             <input type="text" placeholder="Ingrese Nombre de producto" name="nombre"/><br/>
             <input type="number" placeholder="Precio" name="precio"/><br/>
             Disponible<input type="checkbox" name="disponible"/><br/>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Registrar" />
           </form>
         </div>
   
+  {/*
         <div className="ver-resultados">
           <ul>
             {this.state.productos.map(producto => <li>{producto.codigo} - {producto.nombre} - {producto.precio} - Disponible: {producto.disponible?'SI':'NO'}</li>)}
           </ul>
         </div>
+  */}
         
       </div>
     );
